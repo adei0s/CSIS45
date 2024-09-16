a22727b1a80b33d92234776664708faa11463c32
Tingting Li <67816972+adei0s@users.noreply.github.com>
2024-09-16 19:42:45 +0000

    class_9_16

---
+++ class_9_16.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    string names[] = {"Dom", "Ed", "Tingting", "Albert"};
+    char grades[] = {'B', 'C', 'A', 'B'};
+ 
+
+    for (int i=0; i<4; i++){
+        cout << names[i] << endl;
+    }
+
+}
