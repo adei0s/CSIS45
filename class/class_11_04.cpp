29a92e784562fdff395675556d6324fd0476976d
Tingting Li <67816972+adei0s@users.noreply.github.com>
2024-11-04 20:46:45 +0000

    class multi array and vector

---
+++ class/class_11_04.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binarySearch (int arr[], int size, int target) {
+    int left = 0;
+    int right = size - 1 ;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target) {
+        return mid;
+        }
+
+        else if (target < arr[mid]) {
+            right = mid - 1;
+        }
+
+        else if (target < arr[mid]) {
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+
+int main() {
+
+    int multi_array[3][5] = { {1, 2, 3, 4, 5},
+                              {6, 7, 8, 9, 10},
+                              {11, 12, 13, 14, 15}
+    };
+
+    for (int i=0; i<3; i++) {
+        for (int j=0; j<5; j++) {
+            cout << multi_array[i][j] << endl;
+
+        }
+    }
+
+    vector<int> num = {}
+
+}
